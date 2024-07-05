35d7c39df023913187189deb2af736e020d820b0
roberto-fernandino <romfernandino@gmail.com>
2024-07-05 14:37:47 -0300

    feat: Open the event creator modal clicking in a day

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,7 +1,7 @@
 import React from "react";
 import dayjs from "dayjs";
 
-export default function Day({ day, idx }) {
+export default function Day({ day, idx, setEventModalOpen }) {
   function getCurrentDayClass() {
     return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
       ? "bg-blue-700 text-white rounded-full"
@@ -9,10 +9,18 @@ export default function Day({ day, idx }) {
   }
   return (
     <div className="flex flex-col justify-center items-center border-[1px] border-gray-200 ">
+
       {idx === 0 && (
-        <div className="p-3 mt-[-3rem] mb-[2rem]"> {day.format("dddd")} </div>
+        <div className="p-3 mt-[-1rem] mb-[1rem]"> {day.format("dddd")} </div>
       )}
-      <p className={`${getCurrentDayClass()} p-1 `}>{day.format("DD")}</p>
+
+      <p
+        className={`${getCurrentDayClass()} p-3 cursor-pointer hover:bg-gray-700 rounded-full`}
+        onClick={() => setEventModalOpen(true)}
+      >
+        {day.format("DD")}
+      </p>
+
     </div>
   );
 }
