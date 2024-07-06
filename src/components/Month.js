3d12987ce4509b97b607dc51189815d2a572ab84
roberto-fernandino <romfernandino@gmail.com>
2024-07-06 12:26:33 -0300

    chore: Update Month component to include setEventModalOpen prop

--- src/components/Month.js
+++ src/components/Month.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Day from "./Day";
-export default function Month({ month }) {
+export default function Month({ month, setEventModalOpen }) {
   return (
     <div className="flex-1 grid grid-cols-7 grid-rows-5">
       {month.map((row, idx) => (
         <React.Fragment key={idx}>
           {row.map((day, idxx) => (
-            <Day day={day} key={idxx} idx={idx} />
+            <Day day={day} key={idxx} idx={idx} setEventModalOpen={setEventModalOpen}/>
           ))}
         </React.Fragment>
       ))}
