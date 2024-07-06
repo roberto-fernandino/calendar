e8ed00f46fb4c3d0c7cc2192c3a8023036ece96a
roberto-fernandino <romfernandino@gmail.com>
2024-07-06 17:35:25 -0300

    chore: Update Day component to include event handling and styling improvements

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,26 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dayjs from "dayjs";
 
-export default function Day({ day, idx, setEventModalOpen }) {
+export default function Day({ day, idx, handleEventModalOpen }) {
+  const [events, setEvents] = useState([]);
+
   function getCurrentDayClass() {
     return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
       ? "bg-blue-700 text-white rounded-full"
       : "";
   }
-  return (
-    <div className="flex flex-col justify-center items-center border-[1px] border-gray-200 ">
 
-      {idx === 0 && (
-        <div className="p-3 mt-[-1rem] mb-[1rem]"> {day.format("dddd")} </div>
-      )}
+  function setEventColor(id, event) {
+    const eventDiv = document.getElementById(id);
+    eventDiv.style.backgroundColor = event.color;
+  }
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedEvents = localStorage.getItem("events");
+      setEvents(storedEvents ? JSON.parse(storedEvents) : []);
+    }
+  }, []);
+
+  useEffect(() => {
+    events.forEach((event, idx) => {
+      if (event && event.date === day.format("YYYY-MM-DD")) {
+        const id = `${event.date}-${idx}`;
+        setEventColor(id, event);
+      }
+    });
+  }, [events, day]);
+
+  return (
+    <div className="flex flex-col justify-start items-center border-[1px] border-gray-200 ">
+      {idx === 0 && <div className="p-2"> {day.format("dddd")} </div>}
 
       <p
         className={`${getCurrentDayClass()} p-3 cursor-pointer hover:bg-gray-700 rounded-full`}
-        onClick={() => setEventModalOpen(true)}
+        onClick={() => handleEventModalOpen(true, day)}
       >
         {day.format("DD")}
       </p>
-
+      {events &&
+        events
+          .filter((event) => event != null)
+          .map((event, idx) => {
+            if (event.date === day.format("YYYY-MM-DD")) {
+              return (
+                <div
+                  id={`${event.date}-${idx}`}
+                  key={idx}
+                  className={`p-2 rounded-md bg-[${event.color}] text-sm flex flex-row items-center justify-between w-full`}
+                >
+                  {event.title}
+                  {event.icon && (
+                    <img
+                      src={`${event.icon}`}
+                      alt="weather icon"
+                      className="w-7"
+                    />
+                  )}
+                </div>
+              );
+            }
+          })}
     </div>
   );
 }
