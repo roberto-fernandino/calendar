16269d773f1989394a1bb72cf45ce908b24a1590
roberto-fernandino <romfernandino@gmail.com>
2024-07-06 17:35:20 -0300

    chore: Update page.js to include current date, month, and year states and event modal functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,62 @@
-"use client"; 
+"use client";
 import { useState } from "react";
-import { getMonth, getMonthString } from "../../lib/util";
+import { getMonth } from "../../lib/util";
 import Month from "@/components/Month";
 import Header from "@/components/Header";
 import dayjs from "dayjs";
 import { EventModal } from "@/components/Modal";
+import { saveEvent } from "../../lib/util";
+
 /* @client */
 export default function Home() {
-  const [currentMonth, setCurrentMonth] = useState(getMonth());
-  const [monthString, setMonthString] = useState(getMonthString());
+  // State for the current date ojbect
+  const [currentDate, setCurrentDate] = useState(dayjs());
+
+  // State for the current month matriz
+  const [currentMonth, setCurrentMonth] = useState(
+    getMonth(dayjs().month(), dayjs().year())
+  );
+
+  // State for the current month number
   const [monthNumber, setMonthNumber] = useState(dayjs().month());
+
+  // States for the event modal
   const [isEventModalOpen, setEventModalOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  // State for the current year
+  const [currentYear, setCurrentYear] = useState(dayjs().year());
+
+  const handleEventModalOpen = (isOpen, date) => {
+    setSelectedDate(date);
+    setEventModalOpen(isOpen);
+  };
 
   return (
     <>
       <main className="h-screen flex flex-col">
         <Header
-          month={monthString}
-          func={setCurrentMonth}
+          currentDate={currentDate}
+          setCurrentDate={setCurrentDate}
+          year={currentYear}
+          setCurrentMonth={setCurrentMonth}
           setNumber={setMonthNumber}
-          setMonthString={setMonthString}
           monthNumber={monthNumber}
+          setCurrentYear={setCurrentYear}
         />
         <div className="flex flex-1">
-          <Month month={currentMonth} setEventModalOpen={setEventModalOpen}/>
-          <EventModal isOpen={isEventModalOpen} onClose={() => setEventModalOpen(false)}/>
+          <Month
+            month={currentMonth}
+            handleEventModalOpen={handleEventModalOpen}
+          />
+          <EventModal
+            isOpen={isEventModalOpen}
+            onClose={() => setEventModalOpen(false)}
+            onSave={saveEvent}
+            date={selectedDate}
+          />
         </div>
       </main>
     </>
   );
-} 
+}
