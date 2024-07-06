2c6ccfdda36db45c29f9f0da53544dcfc4c8c1a2
roberto-fernandino <romfernandino@gmail.com>
2024-07-06 17:35:40 -0300

    Refactor Month component to use handleEventModalOpen prop

--- src/components/Month.js
+++ src/components/Month.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Day from "./Day";
-export default function Month({ month, setEventModalOpen }) {
+export default function Month({ month, handleEventModalOpen }) {
   return (
     <div className="flex-1 grid grid-cols-7 grid-rows-5">
       {month.map((row, idx) => (
         <React.Fragment key={idx}>
           {row.map((day, idxx) => (
-            <Day day={day} key={idxx} idx={idx} setEventModalOpen={setEventModalOpen}/>
+            <Day
+              day={day}
+              key={idxx}
+              idx={idx}
+              handleEventModalOpen={handleEventModalOpen}
+            />
           ))}
         </React.Fragment>
       ))}
