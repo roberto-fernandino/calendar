5f5f994aaad161940e3d01c649c48b841b9706e4
roberto-fernandino <romfernandino@gmail.com>
2024-07-10 00:20:02 -0300

    feat: Improved calendar Header UI, made it more clean and legible.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { CaretLeft, CaretRight } from "@phosphor-icons/react";
 import { getMonth } from "../../lib/util";
-
+import dayjs from "dayjs";
 export default function Header({
   setCurrentMonth,
   setMonthString,
@@ -9,6 +9,21 @@ export default function Header({
   setCurrentDate,
   setCurrentYear,
 }) {
+  const [currentTime, setCurrentTime] = useState(dayjs());
+  const [clientTime, setClientTime] = useState(null);
+
+  useEffect(() => {
+    setCurrentTime(dayjs());
+  }, []);
+
+  useEffect(() => {
+    setClientTime(dayjs());
+    const interval = setInterval(() => {
+      setClientTime((prevTime) => prevTime.add(1, "second"));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   const updateMonth = useCallback(
     (direction) => {
       setCurrentDate((prevDate) => {
@@ -25,15 +40,23 @@ export default function Header({
   return (
     <>
       <main className="flex justify-center w-screen">
-        <div className="flex flex-row justify-between w-screen">
+        <div className="flex flex-row justify-between items-center w-screen">
           <CaretLeft
             size={32}
             onClick={() => updateMonth(-1)}
             className="cursor-pointer"
           />
-          <p className="select-none">
-            Calendar {currentDate.format("MMMM YYYY")}
-          </p>
+          <div className="flex flex-col justify-center items-center">
+            <p className="select-none text-2xl font-bold p-3 text-center">
+              Calendar <br />
+              {currentDate.format("MMMM YYYY")}
+            </p>
+            {clientTime && (
+              <p className="select-none text-sm text-white font-bold text-[1.5rem] mb-2 mt-[-3px]">
+                {clientTime.format("MM/DD/YYYY HH:mm:ss")}
+              </p>
+            )}
+          </div>
           <CaretRight
             size={32}
             onClick={() => updateMonth(1)}
